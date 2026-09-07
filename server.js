926e328625d712550e83ca802d1d7d4f065331d1
xrylsh <anibandarylrashell@gmail.com>
2026-09-08 01:28:58 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require ("cors")
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
@@ -49,6 +54,7 @@ app.get("/", function(req, res) {
     });
 
 });
+
 
 // =====================================
 // SAVE THOUGHT
